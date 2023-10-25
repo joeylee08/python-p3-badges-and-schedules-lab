0f464db5cd873ccca89fb65c607ff2c619cef6b2
Joseph <josephylee72@gmail.com>
2023-10-25 14:12:08 -0700

    completed badges and schedules

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,13 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [badge_maker(name) for name in names]
 
-def assign_rooms(names):
-    return None
+def assign_rooms(names : list):
+    i = 0
+    return [f"Hello, {name}! You'll be assigned to room {names.index(name) + 1}!" for name in names]
 
 def printer(names):
-    return None
+    [print(item) for item in batch_badge_creator(names)]
+    [print(item) for item in assign_rooms(names)]
